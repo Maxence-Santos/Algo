837f2d2d6b34505e23bf13efd3d5cf0690806e4d
SHuttooo <164526241+SHuttooo@users.noreply.github.com>
2025-03-15 17:52:33 +0100

    Update algo_II.c VFinal

--- Projet/algo_II.c
+++ Projet/algo_II.c
@@ -10,47 +10,79 @@ int minimum(int a, int b) {
     return b;
 }
 
+void afficher(int **T, int s, int i) {
+    printf("\n");
+    printf("Tableau (X = Inf) : \n");
+    printf("i/s ");
+    for (int j= 0;j<s+1;j++) {
+        printf("%d ", j);
+    }
+    printf("\n");
+    for (int j=0;j<i+1;j++) {
+        printf("%d : ", j);
+        for (int k=0;k<s+1;k++) {
+            if (T[k][j] == INT_MAX) {
+                printf("X ");
+            }
+            else {
+            printf("%d ", T[k][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main() {
     FILE *fichier;
     fichier = fopen("donnees.txt", "r");
     int ligne;
     fscanf(fichier, "%d", &ligne);
-    int s = ligne;
+    int s = ligne;//151;
+    printf("S = %d\n", s);
     fscanf(fichier, "%d", &ligne);
-    int i = ligne;
+    int i = ligne;//3;
+    printf("i = %d\n", i);
     int *V = (int *)malloc(i * sizeof(int));
     for (int j = 0; j < i; j++) {
         fscanf(fichier, "%d", &ligne);
         V[j] = ligne;
 
     }
+    int **T = (int **)malloc((s+1) * sizeof(int*));
+    for (int j=0;j<s+1;j++) {
+        T[j] = (int *)malloc((i+1) * sizeof(int));
+    }
     for (int j = 0; j < i; j++) {
         printf("V[%d] : %d\n", j, V[j]);
     }
-    int **T = (int **)malloc(s+1 * sizeof(int*));
-    for (int j=0;j<s+1;j++) {
-        T[j] = (int *)malloc(i+1 * sizeof(int));
-    }
+
     int min1 = 0;
     int min2 = 0;
     for (int j=0;j<s+1;j++) {
-        T[j][0] = INT_MAX;
+        for (int k=0;k<i+1;k++) {
+            T[j][k] = INT_MAX;
+        }
     }
     for (int j=0;j<i+1;j++) {
         T[0][j] = 0;
     }
-    for (int j=0;j<s;j++) {
-        for (int k=0;k<i;k++) {
-            if (s-V[k-1] < 0) {
+
+    for (int j=1;j<s+1;j++) {
+        for (int k=1;k<i+1;k++) {
+            if (j-V[k-1] < 0) {
                 min1 = INT_MAX;
             }
             else {
-                min1 = T[s-V[k]][k]+1;
+                min1 = T[j-V[k-1]][k]+1;
             }
-            min2 = T[s][k-1];
-            T[s][k] = minimum(min1,min2);
+            min2 = T[j][k-1];
+            T[j][k] = minimum(min1,min2);
         }
     }
+
+    afficher(T,s,i);
+
     int s1 =s;
     int i1 = i;
     int* A = (int *)malloc(i1 * sizeof(int));
@@ -59,14 +91,17 @@ int main() {
     }
     while (s1 != 0 && i1 != 0) {
         if (T[s1][i1] != T[s1][i1-1]) {
-            A[i1]++;
+            A[i1-1]++;
             s1 -= V[i1-1];
-            printf("bgf"); 
         }
         else {
             i1--;
         }
     }
-    printf("A[k] : %d", A[i1]);
+    printf("Il faut : %d pots pour S = %d et i = %d\n", T[s][i], s, i);
+    printf("Il y a : \n");
+    for (int j=0;j<i;j++) {
+        printf("- %d pot V[%d] (%d)\n", A[j], j, V[j]);
+    }
     return 0;
 }
